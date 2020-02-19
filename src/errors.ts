8c69ae3e37e646fd64ed391cccc5a81ec195cad4
corno <github.a@schraverus.com>
2020-02-19 09:36:28 +0100

    added original message

--- src/errors.ts
+++ src/errors.ts
@@ -5,16 +5,20 @@ import { Range, printRange, Location, printLocation } from "./location";
 
 export class RangeError extends Error {
     public readonly range: Range
+    public readonly rangeLessMessage: string
     constructor(message: string, range: Range) {
         super(`${message} @ ${printRange(range)}`)
+        this.rangeLessMessage = message
         this.range = range
     }
 }
 
 export class LocationError extends Error {
     public readonly location: Location
+    public readonly locationLessMessage: string
     constructor(message: string, location: Location) {
         super(`${message} @ ${printLocation(location)}`)
         this.location = location
+        this.locationLessMessage = message
     }
 }
