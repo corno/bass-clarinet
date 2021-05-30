792ea09f34fb464ef83244f1b12236d54593c473
corno <gitkraken.a@schraverus.com>
2021-05-30 21:56:55 +0200

    whitespace

--- src/expect/index.ts
+++ src/expect/index.ts
@@ -1,7 +1,5 @@
 export * from "./api"
 
-
-
 export { printExpectError } from "./implementation/print"
 export { printExpectErrorValueType } from "./implementation/print"
 export { printExpectErrorValue } from "./implementation/print"
