9e8eab9230aa5bbb077d3f4d23d499a12e99c2b6
corno <gitkraken.a@schraverus.com>
2021-06-29 21:10:16 +0200

    return type

--- src/interfaces/deserialize/SchemaSchemaBuilder.ts
+++ src/interfaces/deserialize/SchemaSchemaBuilder.ts
@@ -1,8 +1,9 @@
+import * as p from "pareto"
 import { SchemaAndSideEffects } from "./SchemaAndSideEffects"
 import * as astncore from "astn-core"
 import { SchemaDeserializationError } from "./Errors"
 
 export type SchemaSchemaBuilder<Annotation> = (
     onSchemaError: (error: SchemaDeserializationError, annotation: Annotation) => void,
-    onSchema: (schema: SchemaAndSideEffects<Annotation>) => void,
+    onSchema: (schema: SchemaAndSideEffects<Annotation>) => p.IValue<null>,
 ) => astncore.ITreeBuilder<Annotation>
